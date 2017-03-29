8182fc0c1847262367c2f3a068bac041f868f5a6
Beau Van Kirk <beauvankirk@gmail.com>
2017-03-29 17:46:48 -0500

    correct paths

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 export {
   graphql-server-core
-} from './graphql-server-core/dist';
+} from './packages/graphql-server-core/dist';
 
 export {
   graphql-server-express
-} from './graphql-server-express/dist';
+} from './packages/graphql-server-express/dist';
 
 export {
   graphql-server-hapi
-} from './graphql-server-hapi/dist';
+} from './packages/graphql-server-hapi/dist';
 
 export {
   graphql-server-integration-testsuite
-} from './graphql-server-integration-testsuite/dist';
+} from './packages/graphql-server-integration-testsuite/dist';
 
 export {
   graphql-server-koa
-} from './graphql-server-koa/dist';
+} from './packages/graphql-server-koa/dist';
 
 export {
   graphql-server-lambda
-} from './graphql-server-lambda/dist';
+} from './packages/graphql-server-lambda/dist';
 
 export {
   graphql-server-module-graphiql
-} from './graphql-server-module-graphiql/dist';
+} from './packages/graphql-server-module-graphiql/dist';
 
 export {
   graphql-server-module-operation-store
-} from './graphql-server-module-operation-store/dist';
+} from './packages/graphql-server-module-operation-store/dist';
 
 export {
   graphql-server-restify
-} from './graphql-server-restify/dist';
+} from './packages/graphql-server-restify/dist';
