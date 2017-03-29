c43160e85135fae71f103defdd18f74ea1b953b9
Beau Van Kirk <beauvankirk@gmail.com>
2017-03-29 17:32:53 -0500

    repack to be required as git url in package.json

---
+++ index.js
@@ -0,0 +1,35 @@
+export {
+  graphql-server-core
+} from './graphql-server-core/dist';
+
+export {
+  graphql-server-express
+} from './graphql-server-express/dist';
+
+export {
+  graphql-server-hapi
+} from './graphql-server-hapi/dist';
+
+export {
+  graphql-server-integration-testsuite
+} from './graphql-server-integration-testsuite/dist';
+
+export {
+  graphql-server-koa
+} from './graphql-server-koa/dist';
+
+export {
+  graphql-server-lambda
+} from './graphql-server-lambda/dist';
+
+export {
+  graphql-server-module-graphiql
+} from './graphql-server-module-graphiql/dist';
+
+export {
+  graphql-server-module-operation-store
+} from './graphql-server-module-operation-store/dist';
+
+export {
+  graphql-server-restify
+} from './graphql-server-restify/dist';
